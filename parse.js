489786926b7b80e41c6176729fa254cd6daf9ed7
Daniil Kirichenko <daniil@kirick.me>
2025-01-23 21:08:57 +0300

    Update parse.js

--- parse.js
+++ parse.js
@@ -1,16 +1,42 @@
 import { appendFile } from 'node:fs/promises';
 
-console.log(process.env.INPUT_RESULT);
+const results = new Set();
+let job_failed;
 
-// STATUS:
-// 0 - Succeeded
-// 1 - Failed
-// 2 - Skipped
+for (const [ job, { result }] of JSON.parse(process.env.INPUT_RESULT)) {
+	results.add(result);
 
-await appendFile(
-	process.env.GITHUB_OUTPUT,
-	'value=' + JSON.stringify({
-		status: 0,
-		job_name: null,
-	}),
-);
+	if (result === 'failure' && typeof job_failed !== 'string') {
+		job_failed = job;
+	}
+}
+
+if (results.size > 0) {
+	// STATUS:
+	// 0 - Succeeded
+	// 1 - Failed
+	// 2 - Skipped
+	let status;
+	if (results.has('failure')) {
+		status = 1;
+	}
+	else if (results.has('skipped') && results.size === 1) {
+		status = 2;
+	}
+	else if (results.has('success')) {
+		status = 0;
+	}
+	else {
+		console.error('process.env.INPUT_RESULT', process.env.INPUT_RESULT);
+		console.error('results', results);
+		throw new Error('Unknown state found.');
+	}
+	
+	await appendFile(
+		process.env.GITHUB_OUTPUT,
+		'value=' + JSON.stringify({
+			status: 0,
+			job_failed,
+		}),
+	);
+}
