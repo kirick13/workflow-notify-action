4e9f9fe239fee121e30c3dd0b8d056ddbca7c3e5
Daniil Kirichenko <daniil@kirick.me>
2025-01-23 21:56:08 +0300

    Update send-discord.js

--- send-discord.js
+++ send-discord.js
@@ -5,71 +5,66 @@ const {
 	'INPUT_SIGNATURE': signature,
 } = process.env;
 
-// if (
-// 	input?.length > 0
-// 	&& webhook?.length > 0
-// ) {
-	const { repo_name, status, job_failed } = JSON.parse(input);
+const { repo_name, status, job_failed } = JSON.parse(input);
 
-	const COLORS = [
-		0x16a34a, // succeeded
-		0xdc2626, // failed
-		0x525252, // skipped
-	];
-	const TITLES = [
-		'✅ Workflow succeeded!', // succeeded
-		`❌ Workflow failed`, // failed
-		'🗿 Workflow skipped', // skipped
-	];
-	
-	const embed = {
-		color: COLORS[status],
-		title: TITLES[status],
-		author: {
-			name: repo_name,
-			icon_url,
-		},
-		timestamp: new Date().toISOString(),
+const COLORS = [
+	0x16a34a, // succeeded
+	0xdc2626, // failed
+	0x525252, // skipped
+];
+const TITLES = [
+	'✅ Workflow succeeded!', // succeeded
+	`❌ Workflow failed`, // failed
+	'🗿 Workflow skipped', // skipped
+];
+
+const embed = {
+	color: COLORS[status],
+	title: TITLES[status],
+	author: {
+		name: repo_name,
+		icon_url,
+	},
+	timestamp: new Date().toISOString(),
+};
+
+if (status === 1) { // failed
+	embed.description = `Job \`${job_failed}\` failed.`;
+}
+
+if (typeof signature === 'string') {
+	embed.footer = {
+		text: signature,
 	};
+}
 
-	if (status === 1) { // failed
-		embed.description = `Job \`${job_failed}\` failed.`;
-	}
+const body = {
+	embeds: [
+		embed,
+	],
+};
 
-	if (typeof signature === 'string') {
-		embed.footer = {
-			text: signature,
-		};
-	}
+// console.log(JSON.stringify(body, null, 4));
+// console.log();
 
-	const body = {
-		embeds: [
-			embed,
-		],
-	};
-
-	// console.log(JSON.stringify(body, null, 4));
-	// console.log();
-
-	const response = await fetch(
-		webhook,
-		{
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(body),
+const response = await fetch(
+	webhook,
+	{
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
 		},
-	);
+		body: JSON.stringify(body),
+	},
+);
 
-	// console.log(response.status, response.statusText);
-	// console.log(
-	// 	[ ...response.headers.entries() ]
-	// 		.map(([ k, v ]) => k + ': ' + v)
-	// 		.join('\n'),
-	// );
-	// console.log();
-	// console.log(
-	// 	await response.text(),
-	// );
-// }
+// console.log(response.status, response.statusText);
+// console.log(
+// 	[ ...response.headers.entries() ]
+// 		.map(([ k, v ]) => k + ': ' + v)
+// 		.join('\n'),
+// );
+// console.log();
+// console.log(
+// 	await response.text(),
+// );
